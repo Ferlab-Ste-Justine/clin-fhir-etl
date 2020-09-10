b91f0cdcd8596055fd8e3d5230e6f2a27eb06f05
YanLabCHU <ymoussaoui@ferlab.bio>
2020-09-10 13:59:13 -0400

    Adds Logger

---
+++ src/Logger.ts
@@ -0,0 +1,77 @@
+import chalk from "chalk";
+
+interface Logger {
+	info(message: string): void;
+	warn(message: string): void;
+	error(message: string): void;
+}
+
+class ConsoleLogger implements Logger {
+	constructor(private readonly namespace: string){}
+
+	info(message: string): void {
+		console.log(`${chalk.blue(chalk.bold(this.namespace))} ${chalk.blue(message)}`); 
+	}
+	warn(message: string): void {
+		console.log(`${chalk.yellow(chalk.bold(this.namespace))} ${chalk.yellow(message)}`); 
+	}
+	error(message: string): void {
+		console.log(`${chalk.red(chalk.bold(this.namespace))} ${chalk.red(message)}`); 
+	}
+}
+
+enum LoggerType {
+	CONSOLE,
+	FILE,
+	OUTPUT
+}
+
+type LoggerEntry = {
+	type: LoggerType;
+	name: string;
+	alias?: string;
+}
+
+export class AppLogger {
+	private static readonly loggers: { [key: string]: Logger } = {};
+
+	public static init(...types: LoggerEntry[]): void {
+		types.forEach(entry => {
+			const logger = this.createLogger(entry);
+			this.loggers[entry.name] = logger;
+			if(entry.alias != null){
+				this.loggers[entry.alias] = logger;
+			}
+		});
+	}
+
+	private static createLogger(entry: LoggerEntry): Logger {
+		switch (entry.type) {
+		case LoggerType.CONSOLE:
+			return new ConsoleLogger(entry.name);
+
+		default:
+			throw new Error();
+		}
+	}
+
+	public static of(namespace: string): Logger {
+		return this.loggers[namespace];
+	}
+}
+
+export const initLoggers = () : void => {
+	AppLogger.init({
+		name: "Main",
+		alias: "main",
+		type: LoggerType.CONSOLE
+	},{
+		name:	"FHIR Api",
+		alias: "api",
+		type: LoggerType.CONSOLE
+	},{
+		name: "Network",
+		alias: "net",
+		type: LoggerType.CONSOLE
+	});
+};
