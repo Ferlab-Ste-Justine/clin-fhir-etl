60902478e3ff8b38a22f46c380f366ddd535be76
YanLabCHU <ymoussaoui@ferlab.bio>
2020-09-14 13:09:08 -0400

    Add comments

--- src/parsers/Parser.ts
+++ src/parsers/Parser.ts
@@ -3,19 +3,33 @@ import { SheetPage, ParsedType, ResourceType, Reference } from '../data/Data';
 
 export abstract class Parser<T extends ParsedType> {
     protected rawData: string[][];
-
     protected parsedData: T[];
 
     public get parsed(): T[] {
         return this.parsedData;
     }
 
+    /**
+     * Other pages that are referenced to from this document. 
+     */
     public abstract get dependencies(): SheetPage[];
 
+    /**
+     * Type parsed by the parser.
+     */
     public abstract get sheetType(): SheetPage;
 
+    /**
+     * Takes a list of string and parse an object of type @type T
+     * @param row List of string data of the row
+     */
     public abstract parseRow (row: string[]): T;
 
+    /**
+     * Iterate through each line in @param data and parse an object @type T.
+     * The parsed data is then stored in the @property parsedData.
+     * @param data All the lines to parse
+     */
     public parse(data: string[][]): void {
         this.rawData = data;
         this.parsedData = [];
